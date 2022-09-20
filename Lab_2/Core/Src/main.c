96a4273c10b28e230a735957cfa38f78657a383a
kryllcowboy <kirill77577@gmail.com>
2022-09-20 15:11:55 +0300

    lab_2_correct

--- Lab_2/Core/Src/main.c
+++ Lab_2/Core/Src/main.c
@@ -100,7 +100,7 @@ int main(void)
     HAL_GPIO_WritePin(LED_ORANGE_GPIO_Port, LED_ORANGE_Pin, GPIO_PIN_SET);
     
     HAL_Delay(400);
-    HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(LED_BLUE_GPIO_Port, LED_BLUE_Pin, GPIO_PIN_RESET);
     
     HAL_Delay(200);
