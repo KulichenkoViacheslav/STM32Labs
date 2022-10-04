07b74d17967210d0fe79b21f1af2aeb7b391e977
Viacheslav Kulichenko <48729747+KulichenkoViacheslav@users.noreply.github.com>
2022-10-04 15:54:50 +0300

    Created Lab_HC_SR04

---
+++ LAB_HC_SR04/MDK-ARM/RTE/_LAB_HC_SR04/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LAB_HC_SR04' 
+ * Target:  'LAB_HC_SR04' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
